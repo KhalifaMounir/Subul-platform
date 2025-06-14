4550c386b2c6b0d7e74040a6ffabf8d2cab7d2fc
zeinebroihii <100594821+zeinebroihii@users.noreply.github.com>
2025-06-15 00:16:39 +0100

    fix of imports config

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_migrate import Migrate
-from backend.config import Config
+from config import Config
 
 db = SQLAlchemy()
 login_manager = LoginManager()
